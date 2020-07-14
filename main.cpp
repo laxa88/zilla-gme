f0c6fad2b67ee200e57115957037505ab84f32e9
LWY <668915+laxa88@users.noreply.github.com>
2020-07-14 23:32:31 +0900

    cleanup main, add zelda.nsf

--- main.cpp
+++ main.cpp
@@ -16,28 +16,11 @@ static Audio_Scope* scope;
 static Music_Player* player;
 static short scope_buf[scope_width * 2];
 
-//bool mixer(short* buffer, unsigned int samples, bool need_mix)
-//{
-//	int len = (samples << 1);
-//
-//	for (int i = 0; i < len; i++) {
-//		buffer[i] = RAND_RANGE(INT8_MIN, INT8_MAX);
-//	}
-//
-//	return need_mix;
-//}
-
 static void init()
 {
 	ZL_Display::Init("Zilla GME", scope_width, 256);
 	ZL_Display::SetAA(false);
 	ZL_Input::Init();
-
-	// Start SDL
-	/*if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
-		exit(EXIT_FAILURE);
-	atexit(SDL_Quit);
-	SDL_EnableKeyRepeat(500, 80);*/
 
 	// Init scope
 	scope = new Audio_Scope;
@@ -60,7 +43,7 @@ static void start_track(int track, const char* path)
 	paused = false;
 	handle_error(player->start_track(track - 1));
 
-	// update window title with track info
+	// update track info
 
 	long seconds = player->track_info().length / 1000;
 	const char* game = player->track_info().game;
@@ -76,11 +59,11 @@ static void start_track(int track, const char* path)
 			game++; // skip path separator
 	}
 
-	char title[512];
-	sprintf(title, "%s: %d/%d %s (%ld:%02ld)",
+	ZL_String::format("%s: %d/%d %s (%ld:%02ld)",
 		game, track, player->track_count(), player->track_info().song,
 		seconds / 60, seconds % 60);
-	//SDL_WM_SetCaption(title, title);
+
+	// TODO display text in window
 }
 
 static struct sZillaGME : public ZL_Application
@@ -102,21 +85,9 @@ static struct sZillaGME : public ZL_Application
 		
 		// Load file
 		//path = (argc > 1 ? argv[argc - 1] : "Data/test.nsf");
-		path = (argc > 1 ? argv[argc - 1] : "Data/smb1.nsf");
+		path = (argc > 1 ? argv[argc - 1] : "Data/zelda.nsf");
 		handle_error(player->load_file(path));
 		start_track(1, path);
-
-		//srfBuffer = ZL_Surface(256, 256); //initialize a 256x256 render target texture
-		//srfBuffer.RenderToBegin(); //start drawing onto the render target texture
-		//ZL_Display::ClearFill(ZL_Color::Red); //clear the texture to fully red
-		//ZL_Surface srfLogo("Data/ZILLALIB.png"); //load another surface texture
-		//srfLogo.DrawTo(10.0f, 10.0f, 90.0f, 90.0f); //draw the newly loaded surface texture into the buffer
-		//srfLogo.DrawTo(70.0f, 70.0f, 150.0f, 150.0f); //again
-		//srfLogo.DrawTo(130.0f, 130.0f, 210.0f, 210.0f); //again
-		//srfBuffer.RenderToEnd(); //end drawing to the texture
-
-		/*ZL_Audio::Init();
-		ZL_Audio::HookAudioMix(&mixer);*/
 	}
 
 	void AfterFrame()
@@ -132,29 +103,6 @@ static struct sZillaGME : public ZL_Application
 			else
 				player->pause(paused = true);
 		}
-
-
-
-
-
-
-		//ZL_Vector SurfacePosition(299.0f, 112.0f);
-		//if (ZL_Input::Held())
-		//{
-		//	//Draw into the buffer while the mouse is pressed, draw in 1 pixel steps from the old mouse position to the current position
-		//	ZL_Vector MousePosInImage = ZL_Input::Pointer() - SurfacePosition, MouseMoveDir = ZL_Input::PointerDelta().VecNorm();
-		//	scalar MouseMoveTotal = (ZL_Input::Down() ? 0.0f : ZL_Math::Max(ZL_Input::PointerDelta().GetLength() - 1.0f, 0.0f));
-		//	srfBuffer.RenderToBegin();
-		//	for (scalar i = 0; i <= MouseMoveTotal; i++) {
-		//		//ZL_Display::FillCircle(MousePosInImage - MouseMoveDir * i, 1.0f, ZL_Color::Blue);
-		//		
-		//	}
-		//	ZL_Display::DrawLine(MousePosInImage, MousePosInImage - ZL_Input::PointerDelta(), ZL_Color::White);
-		//	srfBuffer.RenderToEnd();
-		//}
-
-		//ZL_Display::ClearFill(ZL_Color::Black); //clear whole screen
-		//srfBuffer.Draw(SurfacePosition);
 	}
 } ZillaGME;
 
@@ -162,20 +110,5 @@ static struct sZillaGME : public ZL_Application
 void handle_error(const char* error)
 {
 	if (error)
-	{
-		// put error in window title
-		char str[256];
-		sprintf(str, "Error: %s", error);
-		fprintf(stderr, str);
-		//SDL_WM_SetCaption(str, str);
-
-		// wait for keyboard or mouse activity
-		/*SDL_Event e;
-		do
-		{
-			while (!SDL_PollEvent(&e)) {}
-		} while (e.type != SDL_QUIT && e.type != SDL_KEYDOWN && e.type != SDL_MOUSEBUTTONDOWN);*/
-
-		//exit(EXIT_FAILURE);
-	}
+		ZL_LOG0("Error", "%s", error);
 }
